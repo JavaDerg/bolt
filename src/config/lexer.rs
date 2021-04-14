ef3f3225342e4037a988e59688d6a2fa5511b14f
postrex <post-rex@pm.me>
2021-04-14 16:34:44 +0200

    Updated config format

--- src/config/lexer.rs
+++
@@ -1,8 +0,0 @@
-use nom::multi::many0;
-use nom::IResult;
-use nom::bytes::complete::tag;
-
-pub fn parse(cfg_str: &str) {
-    let token: IResult<&str, Vec<&str>> = many0(tag("http"))(cfg_str);
-    println!("{:#?}", token.unwrap());
-}
