98db397919a09492d34fb29434bd76c3de4e083a
post-rex <post-rex@pm.me>
2021-05-14 19:12:22 +0200

    Finished tokenizer

--- src/config/parser/mod.rs
+++ src/config/parser/mod.rs
@@ -4,16 +4,32 @@ use std::mem::swap;
 
 use nom::AsChar;
 use smallvec::SmallVec;
+use std::borrow::Cow;
+use std::hint::unreachable_unchecked;
+use std::iter::{FromIterator, Peekable};
+use std::num::ParseIntError;
+use std::str::Chars;
 
 #[derive(Debug)]
-pub enum Token {}
+pub enum Token<'a> {
+    Statement(&'a str),
+    Prefix(&'a str),
+    Numeral(u64),
+    String { content: Cow<'a, str>, format: bool },
+    Dot,
+    Equality,
+    EqualitySwitch(EqualityType),
+    Block(BlockType),
+    Spacer,
+    NewLine,
+}
 
 struct ParseUtil<'a> {
     pub src: &'a str,
     pub len_utf: usize,
     pub state: ParserState,
     pub current: usize,
-    pub tokens: Vec<Token>,
+    pub tokens: Vec<Token<'a>>,
 }
 
 enum ParserState {
@@ -36,21 +52,22 @@ enum ParserState {
         escapes: SmallVec<[usize; 4]>,
         kind: StringType,
     },
-    Separator,
-    Indicator(IndicatorType),
+    Dot,
+    EqualitySwitch(EqualityType),
     Block(BlockType),
     Spacer,
     NewLine,
 }
-#[derive(Eq, PartialEq)]
-enum IndicatorType {
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum EqualityType {
     Equal,
     Regex,
     BeginsWith,
 }
 
-#[derive(Eq, PartialEq)]
-enum BlockType {
+#[derive(Debug, Eq, PartialEq)]
+pub enum BlockType {
     Open,
     Close,
 }
@@ -66,6 +83,9 @@ enum StringType {
 pub enum ParseError {
     UnexpectedCharacter(char),
     EarlyEof,
+    NotANumber,
+    IntParseError(ParseIntError),
+    InvalidCharacterCode(u64),
 }
 
 pub fn parse(src: &str) -> Result<Vec<Token>, ParseError> {
@@ -83,7 +103,7 @@ pub fn parse(src: &str) -> Result<Vec<Token>, ParseError> {
                     util.submit(ParserState::Prefix {
                         start: util.current,
                         end: util.current,
-                    });
+                    })?;
                 }
                 if util.state.is_prefix() {
                     continue;
@@ -92,7 +112,7 @@ pub fn parse(src: &str) -> Result<Vec<Token>, ParseError> {
                     util.submit(ParserState::Statement {
                         start: util.current,
                         end: util.current,
-                    });
+                    })?;
                 }
             }
             '0'..='9' => {
@@ -100,7 +120,7 @@ pub fn parse(src: &str) -> Result<Vec<Token>, ParseError> {
                     util.submit(ParserState::Numeral {
                         start: util.current,
                         end: util.current,
-                    });
+                    })?;
                 }
             }
             '"' => {
@@ -159,7 +179,7 @@ pub fn parse(src: &str) -> Result<Vec<Token>, ParseError> {
                                 end: util.current,
                                 escapes,
                                 kind: StringType::DoubleQuote,
-                            });
+                            })?;
                             invalid = false;
                             break;
                         }
@@ -186,7 +206,7 @@ pub fn parse(src: &str) -> Result<Vec<Token>, ParseError> {
                             end: util.current,
                             escapes,
                             kind: StringType::SingleQuote,
-                        });
+                        })?;
                         invalid = false;
                         break;
                     }
@@ -195,23 +215,25 @@ pub fn parse(src: &str) -> Result<Vec<Token>, ParseError> {
                     return Err(ParseError::EarlyEof);
                 }
             }
-            '.' => util.submit(ParserState::Separator),
-            '=' | '~' | '^' => util.submit(ParserState::Indicator(IndicatorType::from(next))),
+            '.' => util.submit(ParserState::Dot)?,
+            '=' | '~' | '^' => {
+                util.submit(ParserState::EqualitySwitch(EqualityType::from(next)))?
+            }
             '{' | '}' => util.submit(ParserState::Block(if next == '{' {
                 BlockType::Open
             } else {
                 BlockType::Close
-            })),
+            }))?,
             '\r' => {
                 if let Some('\n') = util.peek() {
                     util.advance();
                 }
-                util.submit(ParserState::NewLine);
+                util.submit(ParserState::NewLine)?;
             }
-            '\n' => util.submit(ParserState::NewLine),
+            '\n' => util.submit(ParserState::NewLine)?,
             _ if next.is_whitespace() => {
                 if !util.state.is_spacer() {
-                    util.submit(ParserState::Spacer);
+                    util.submit(ParserState::Spacer)?;
                 }
             }
             _ => return Err(ParseError::UnexpectedCharacter(next)),
@@ -222,11 +244,12 @@ pub fn parse(src: &str) -> Result<Vec<Token>, ParseError> {
 }
 
 impl<'a> ParseUtil<'a> {
-    pub fn submit(&mut self, next: ParserState) {
+    pub fn submit(&mut self, next: ParserState) -> Result<(), ParseError> {
         self.state.complete(self.current);
-        if let Some(token) = self.state.tokenize(next) {
+        if let Some(token) = self.state.tokenize(next, self.src)? {
             self.tokens.push(token);
         }
+        Ok(())
     }
 
     pub fn is_available(&self) -> bool {
@@ -261,12 +284,94 @@ impl<'a> Iterator for ParseUtil<'a> {
 }
 
 impl ParserState {
-    pub fn tokenize(&mut self, new: Self) -> Option<Token> {
+    pub fn tokenize<'a>(
+        &mut self,
+        new: Self,
+        src: &'a str,
+    ) -> Result<Option<Token<'a>>, ParseError> {
         let old = self.swap(new);
-        match old {
-            Self::None => None,
-            _ => todo!(),
-        }
+        Ok(Some(match old {
+            Self::None => return Ok(None),
+            ParserState::Statement { start, end } => Token::Statement(&src[start..end]),
+            ParserState::Prefix { start, end } => Token::Prefix(&src[start..end]),
+            ParserState::Numeral { start, end } => Token::Numeral(
+                (&src[start..end])
+                    .parse()
+                    .map_err(|_| ParseError::NotANumber)?,
+            ),
+            ParserState::String {
+                start,
+                end,
+                escapes,
+                kind,
+            } => {
+                let str = &src[start + 1..end - 1];
+                if !escapes.is_empty() {
+                    Token::String {
+                        content: Cow::Owned({
+                            let mut str = String::with_capacity(src.len());
+                            let mut last = 0;
+                            for escptr in escapes {
+                                if last < escptr {
+                                    str.push_str(&src[last..escptr]);
+                                    let mut citr =
+                                        (&src[escptr + 1..]).chars().zip(escptr..).peekable();
+                                    last = escptr + 1;
+                                    match citr.next().unwrap() {
+                                        ('0', _) => str.push('\0'),
+                                        ('n', _) => str.push('\n'),
+                                        ('r', _) => str.push('\r'),
+                                        ('t', _) => str.push('\t'),
+                                        ('"', _) => str.push('\"'),
+                                        ('x', ptr) => {
+                                            let code = u16::from_str_radix(&src[ptr..ptr + 2], 16)
+                                                .map_err(ParseError::IntParseError)?
+                                                as u32;
+                                            last += 2;
+                                            str.push(char::from_u32(code).ok_or(
+                                                ParseError::InvalidCharacterCode(code as u64),
+                                            )?)
+                                        }
+                                        ('u', _) => {
+                                            // SAFETY: Validity of syntax has been verified in pre tokenization
+                                            let _ = citr.next().unwrap(); // {
+                                            let (_, start) = citr.next().unwrap();
+                                            let (_, stop) = (&src[start..])
+                                                .chars()
+                                                .zip(start..)
+                                                .find(|(c, _)| *c == '}')
+                                                .unwrap();
+                                            last += 2 + (stop - start);
+                                            let code = u32::from_str_radix(&src[start..stop], 16)
+                                                .map_err(ParseError::IntParseError)?;
+                                            str.push(char::from_u32(code).ok_or(
+                                                ParseError::InvalidCharacterCode(code as u64),
+                                            )?);
+                                        }
+                                        _ => unreachable!(),
+                                    }
+                                }
+                            }
+                            if src.len() > last {
+                                str.push_str(&src[last..]);
+                            }
+                            str
+                        }),
+                        format: false,
+                    }
+                } else {
+                    Token::String {
+                        content: Cow::Borrowed(str),
+                        format: matches!(kind, StringType::DoubleQuote),
+                    }
+                }
+            }
+            ParserState::Dot => Token::Dot,
+            ParserState::EqualitySwitch(switch) => Token::EqualitySwitch(switch),
+            ParserState::Block(kind) => Token::Block(kind),
+            ParserState::Spacer => Token::Spacer,
+            ParserState::NewLine => Token::NewLine,
+        }))
     }
 
     pub fn complete(&mut self, pos: usize) {
@@ -302,18 +407,18 @@ impl ParserState {
 
 impl Display for ParseError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        write!(f, "{:?}", self)
     }
 }
 
 impl Error for ParseError {}
 
-impl From<char> for IndicatorType {
+impl From<char> for EqualityType {
     fn from(c: char) -> Self {
         match c {
-            '=' => IndicatorType::Equal,
-            '~' => IndicatorType::Regex,
-            '^' => IndicatorType::BeginsWith,
+            '=' => EqualityType::Equal,
+            '~' => EqualityType::Regex,
+            '^' => EqualityType::BeginsWith,
             _ => panic!("Invalid indicator char type"),
         }
     }
