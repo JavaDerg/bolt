b2cdc1ea408e51c3c534a348a11c133229e1462a
PostRex <post-rex@pm.me>
2021-11-12 10:54:26 +0100

    added additinal fuzz step

--- crates/bolt_url/fuzz/src/main.rs
+++ crates/bolt_url/fuzz/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
     afl::fuzz!(|data: &[u8]| {
         if let Ok(s) = std::str::from_utf8(data) {
-            let _ = bolt_url::UrlPath::parse(&s);
+            if let Ok(path) = bolt_url::UrlPath::parse(&s) {
+                let _ = path.sanitized_path();
+            }
         }
     });
 }
