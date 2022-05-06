6ba41b6216393c819c41e5347ca9a8279ef07e83
PostRex <post-rex@pm.me>
2022-05-06 11:57:02 +0200

    RegexStrategy Tests

--- crates/bolt_router/src/strategies/pattern.rs
+++ crates/bolt_router/src/strategies/pattern.rs
@@ -50,3 +50,60 @@ impl Builder for RegexStrategyBuilder {
         self
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn builder() {
+        let builder = RegexStrategy::builder();
+
+        assert_eq!(builder.patterns, Vec::<String>::new());
+        assert_eq!(builder.table, Vec::<Slot>::new());
+    }
+
+    #[test]
+    fn builder_add() {
+        let mut builder = RegexStrategy::builder();
+        builder.add("[a-z]{3}", Slot(0));
+
+        assert_eq!(builder.patterns, vec!["[a-z]{3}".to_string()]);
+        assert_eq!(builder.table, vec![Slot(0)]);
+    }
+
+    #[test]
+    fn build() {
+        let strategy = RegexStrategy::builder()
+            .add_owned("[a-z]{3}", Slot(0))
+            .add_owned("[0-9]{3}", Slot(1))
+            .build()
+            .unwrap();
+
+        assert_eq!(
+            strategy.regex_set.patterns(),
+            &["[a-z]{3}".to_string(), "[0-9]{3}".to_string()]
+        );
+        assert_eq!(strategy.table, vec![Slot(0), Slot(1)]);
+    }
+
+    #[test]
+    fn r#match() {
+        let strategy = RegexStrategy::builder()
+            .add_owned("[a-z]{3}", Slot(0))
+            .add_owned("[0-9]{3}", Slot(1))
+            .build()
+            .unwrap();
+
+        assert_eq!(strategy.r#match("abc"), Some(Slot(0)));
+        assert_eq!(strategy.r#match("xyz"), Some(Slot(0)));
+        assert_eq!(strategy.r#match("123"), Some(Slot(1)));
+        assert_eq!(strategy.r#match("789"), Some(Slot(1)));
+
+        assert_eq!(strategy.r#match("ab1"), None);
+        assert_eq!(strategy.r#match("abcd"), None);
+        assert_eq!(strategy.r#match("1234"), None);
+        assert_eq!(strategy.r#match(""), None);
+    }
+}
